c5bfd8f72a4457afbc2ba876020bcb4a17ff6ffb
ffse1704006 <36950238+ffse1704006@users.noreply.github.com>
2018-07-20 15:16:43 +0700

    ui

---
+++ ChuongTrinhQuanLyThuVien.java/src/fasttrackse/quanlythuvien/java/main/QuanLyThuVien.java
@@ -0,0 +1,13 @@
+package fasttrackse.quanlythuvien.java.main;
+
+import fasttrackse.quanlythuvien.java.until.QuanLyThuVienUI;
+
+public class QuanLyThuVien {
+
+	public static void main(String[] args) {
+		QuanLyThuVienUI myUI = new QuanLyThuVienUI("Quản lý thư viện");
+		myUI.showView();
+
+	}
+
+}
