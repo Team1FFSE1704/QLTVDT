514979a85bd44a7b9cfbad4c651680e0bd08ce0e
LeeKharHoongNhat <36950051+LeeKharHoongNhat@users.noreply.github.com>
2018-08-06 15:56:53 +0700

    up

---
+++ Team1_QuanLyThuVienDienTu/src/fasttrackse/team1/qlthuviendientu/entyti/Admin.java
@@ -0,0 +1,49 @@
+package fasttrackse.team1.qlthuviendientu.entyti;
+
+import java.io.Serializable;
+
+public class Admin implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private String pass;
+	private String status;
+
+	public Admin(String name, String pass,String status) {
+		super();
+		this.name = name;
+		this.pass = pass;
+		this.status = status;
+	}
+	public Admin(String name, String pass) {
+		super();
+		this.name = name;
+		this.pass = pass;
+
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
